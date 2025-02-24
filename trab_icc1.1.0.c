82481436a6238952fa791d8f3d854342d93a960d
Lucas André <102180146+lucasprog03@users.noreply.github.com>
2025-02-24 20:31:49 -0300

    Update trab_icc1.1.0.c

--- trab_icc1.1.0.c
+++ trab_icc1.1.0.c
@@ -1,21 +1,3 @@
-/*
-
-Projeto realizado para a disciplina SCC0221 - Introdução à Ciência da Computação I (2023) como parte das atividades avaliativas
-propostas durante o semestre.
-
-Este projeto consiste em um mercadinho que realiza a inserção de produtos, vendas, consultas ao estoque,
-consultas ao saldo, modificação de preços e aumento de estoque a partir de comandos escolhidos pelo usuário na linha de comando.
-
-Autores:    
-            Lucas André - 13673260
-            Eduardo Oliveira - 9013182
-            João Pedro Viguini T. T. Correa - 14675503
-            
-            
-Orientador: Prof. Dr. Rudinei Goularte.
-
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
